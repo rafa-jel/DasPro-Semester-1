0356e42c6f9c44730a82fe08d7de647106b0c141
rafa-jel <rafaradana2@gmail.com>
2024-10-02 13:25:50 +0700

    week 6 percobaan 1

---
+++ Pemilihan2Percobaan1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Pemilihan2Percobaan1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tahun;
+        
+        System.out.print("Masukan tahun = ");
+        tahun = sc.nextInt();
+
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0) {
+                System.out.println(tahun + " merupakan Tahun Kabisat");
+            }
+        } else {
+            System.out.println(tahun + " Bukan tahun kabisat");
+        }
+
+    }
+
+    
+}
