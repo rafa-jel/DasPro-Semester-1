d22796c546d9ac45af897e85a8f3317299b635bb
rafa-jel <rafaradana2@gmail.com>
2024-10-30 15:51:48 +0700

    Week 9 percobaan 3 modifikasi

--- ArrayRataNilai23.java
+++ ArrayRataNilai23.java
@@ -4,16 +4,37 @@ public class ArrayRataNilai23 {
         Scanner sc = new Scanner(System.in);
 
         int[] nilaiMhs = new int[10];
-        double total = 0, rata2;
+        int jumlahLulus = 0, jumlahTdkLulus = 0;
+        double totalLulus = 0, totalTdkLulus = 0, rata2Lulus = 0, rata2TdkLulus = 0;
 
-        for (int i = 0; i < nilaiMhs.length; i++){
+        System.out.print("Masukan jumlah mahasiswa = ");//input jumlah mahasiswa
+        int jumlahMhs = sc.nextInt();
+
+        for (int i = 1; i <= jumlahMhs; i++){ //Input nilai nilai mahasiswa
             System.out.print("Masukkan nilai mahasiswa ke - " + i + " = ");
             nilaiMhs[i] = sc.nextInt();
         }
-        for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
+        for (int i = 1; i <= jumlahMhs; i++) {//penjumlahan total mahasiswa yang lulus dan tidak lulus
+            if (nilaiMhs[i] > 70) {
+                totalLulus++;
+            }
+            else {
+                totalTdkLulus++;
+            }
+            
         }
-        rata2 = total / nilaiMhs.length;
-        System.out.println("Rata - rata nilai = " + rata2);
+        for (int i = 1; i <= jumlahMhs; i++) {// penjumlahan nilai
+            if (nilaiMhs[i] > 70) {
+                jumlahLulus += nilaiMhs[i];
+            }
+            else {
+                jumlahTdkLulus += nilaiMhs[i];
+            }
+        }
+
+        rata2Lulus = jumlahLulus / totalLulus;//proses mencari rata rata
+        rata2TdkLulus = jumlahTdkLulus / totalTdkLulus;
+        System.out.println("Rata - rata nilai mahasiswa yang lulus = " + rata2Lulus);
+        System.out.println("Rata - rata nilai mahasiswa yang tidak lulus = " + rata2TdkLulus);
     }
 }
